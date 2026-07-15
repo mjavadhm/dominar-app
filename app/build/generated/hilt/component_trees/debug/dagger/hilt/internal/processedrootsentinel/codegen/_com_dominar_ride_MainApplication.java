18414809c12c518778703047261a885f6362d16a
Javadhm <smjavad82@gmail.com>
2026-07-15 21:39:54 +0330

    chore: add .gitignore and remove build artifacts

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_dominar_ride_MainApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.dominar.ride.MainApplication"
-)
-public final class _com_dominar_ride_MainApplication {
-}
